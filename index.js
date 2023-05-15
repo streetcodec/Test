e42a66acf7bbbc4402084296e8ee73c29d12da23
streetcodec <streetcodec@gmail.com>
2023-05-15 09:08:35 +0530

    Resizing/Scaling feature added

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const x = document.querySelector('#x');
-        const y = document.querySelector('#y');
-        const z = document.querySelector('#z');
-
-        const modelViewerVariants = document.querySelector("model-viewer#var");
+const modelViewerVariants = document.querySelector("model-viewer#var");
         const select = document.querySelector('#variant');
-        
         modelViewerVariants.addEventListener('load', () => {
 
           const names = modelViewerVariants.availableVariants;
@@ -13,31 +8,43 @@ const x = document.querySelector('#x');
             const option = document.createElement('option');
             option.value = name;
             option.textContent = name;
-            select.appendChild(option);
             
+            select.appendChild(option);
+          ;
           }
-          // Adds a default option. (reverts back to original materials with which the objects were initialized in Blender.)
+          // Adds a default option.
           const option = document.createElement('option');
             option.value = 'default';
             option.textContent = 'Default';
             select.appendChild(option);
         });
         
-        // actually changes color.
+        // actually changes color, and scale.
         select.addEventListener('input', (event) => {
+          
           modelViewerVariants.variantName = event.target.value === 'default' ? null : event.target.value;
+          
+          switch(modelViewerVariants.variantName) // using to scale the images to original size, by relatively comparing it to default image, of 28*21 inches square.  
+          { 
+            case "Hamuman" : updateScaleVar(1.36017 , 1.33);
+                                        break;
+            case "Animals Gond" : updateScaleVar(0.31,0.423);
+                                               break;
+            case "Dancing Krishna With Cows" : updateScaleVar(2.811,2.418);
+                                                                    break;
+            case "White" : break;
+            case "Black" : break;
+            default : updateScaleVar(1.27,1.27);
+          }
+          
+            
+          
+          console.log(modelViewerVariants.variantName);
         });
 
        //for scale
-        const updateScale = () => {
-       modelViewerVariants.scale = `${x.value} ${y.value} ${z.value}`;
-        };
-       x.addEventListener('input', () => {
-         updateScale();
-       });
-      y.addEventListener('input', () => {
-      updateScale();
-        });
-      z.addEventListener('input', () => {
-      updateScale();
-          });
+       function updateScaleVar( p , q )
+       {
+        modelViewerVariants.scale = `${p} ${q} ${1}`; 
+
+       }
